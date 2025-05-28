ff3a0402ca7f54d83afe1ba33424bab4c1c7059b
Andres <andrespardo5151@gmail.com>
2025-05-28 12:15:57 -0500

    Se crea el boton

--- resources/views/mi_unidad/index.blade.php
+++ resources/views/mi_unidad/index.blade.php
@@ -2,5 +2,14 @@
 
     <x-slot name="title">
         {{ __('My Unit') }}
+
+           <div class="flex justify-end">
+            <x-button wire:navigate 
+            href="{{}}"
+             class="mb-4">
+                <x-icon name="folder" class="mr-2" />
+                {{ __('Create Unit') }}
+            </x-button>
+        </div>
     </x-layouts.app>
-    
+
