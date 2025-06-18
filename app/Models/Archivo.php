89250781146c13730b311760627ec8e951f7f8d2
Andres <andrespardo5151@gmail.com>
2025-06-18 07:27:40 -0500

    Se crea la migracion del parametro ruta en la tabla archivo

--- app/Models/Archivo.php
+++ app/Models/Archivo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Archivo extends Model
 {
-    protected $fillable = ['nombre', 'carpeta_id']; // Campos que se llenarán en la base de datos
+    protected $fillable = ['nombre','ruta', 'carpeta_id']; // Campos que se llenarán en la base de datos
 
     public function carpeta() // Relación muchos a uno 
     {
